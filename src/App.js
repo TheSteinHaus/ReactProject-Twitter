d0c00f3a73e9157d63ca07de403564143fe769c6
TheSteinHaus <semen.skopin@gmail.com>
2021-10-29 22:50:03 +1000

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import React, { Children } from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import './App.css';
+import User from './components/posts/posts'
+import Tweet from './components/makeTweet/make'
+import Hat from './components/hat/hat'
+import post from './components/posts/posts.json'
+import RightMenu from './components/right_menu/right_menu';
+import Layout from './components/layout/layout';
+import LeftMenu from './components/left-menu/left_menu';
+import Header from './components/main_header/main_header';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <div className="sides">
+            <Layout>
+              <LeftMenu />
+            </Layout>
+            <div className="main">
+              <Layout>
+                <Header main_text="Главная" />
+              </Layout>
+              <Tweet />
+                { [...Array(post.length)].map((item, index) => <User key={index} date={post[index].date} text={post[index].text} author={post[index].author} /> ) }
+            </div>
+            <RightMenu />
+            
+          </div>
+        </Route>
+        
+        <Route path="/profile">
+          <div className="sides">
+            <Layout>
+              <LeftMenu />
+            </Layout>
+            <div className="main">
+              <Layout>
+                <Header main_text="Профиль" />
+              </Layout>
+              <Hat />
+            </div>
+            <RightMenu />
+            
+          </div>
+        </Route>
+
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
